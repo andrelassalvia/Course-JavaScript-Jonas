b786cd83cf33ed6df70fbbb67dfec9a0d663366f
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-11 19:45:09 +0000

    Argumentos por valor e por referencia

--- Secao10-Funcoes/script.js
+++ Secao10-Funcoes/script.js
@@ -1,24 +1,38 @@
 "use strict";
 
-// Por vezes e interessante que um parametro esteja definido como default
-// Podemos definir como default direto nos parametros
-const bookings = [];
-
-const createBooking = function (flightNum, numPass = 2, price = 199 * numPass) {
-  // podemos passar calculos como default
-  const booking = {
-    flightNum,
-    numPass,
-    price,
-  };
-
-  console.log(booking);
-  bookings.push(booking);
+//Diferenca entre passar argumentos por valor e por referencia
+const flight = "LH1234";
+const jonas = {
+  name: "Jonas Cardoso",
+  passport: 123456789,
 };
 
-createBooking("LH2312"); // como nao especificamos os outros parametros eles estao como indefinidos e os default sao chamados
+const checkIn = function (flightNumber, passenger) {
+  flightNumber = "LA9999";
+  passenger.name = "Mr." + passenger.name;
 
-createBooking("LH2312", 200, 1000);
+  if (passenger.passport === 123456789) {
+    alert("Check In OK");
+  } else {
+    alert("Tenta de novo");
+  }
+};
 
-// se nao quisermos passar o segundo parametro o chamamos como undefined
-createBooking("LH123", undefined, 1000);
+checkIn(flight, jonas);
+console.log(flight); // Resposta: LH1234 - nao alterou o valor original
+console.log(jonas); // Resposta: name: Mr. Jonas Cardoso, Passport: 123456789 - alterou o valor original
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Resumo da opera: objetos sao alterados pois fazem referencia a um ponto da memoria.
+// variaveis primitivas nao sao alteradas pois estao gravados naquele endereco.
+
+// JavaScript nao possui passagem por referencia pois o valor na memoria nao e alterado por referencia.
+// o que acontece com o objeto e que alteramos a referencia de endereco e nao de memoria.
