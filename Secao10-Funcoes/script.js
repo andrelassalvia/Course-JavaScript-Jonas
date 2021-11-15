0aadc093df167e6b471190e90c20b47b44f3dfa7
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-15 19:24:33 +0000

    Funcoes retornando funcoes

--- Secao10-Funcoes/script.js
+++ Secao10-Funcoes/script.js
@@ -1,41 +1,18 @@
 "use strict";
 
-// Usando flag global para retirar espacos entre as palavras
-const oneWord = function (str) {
-  return str.replace(/ /g, "").toLowerCase();
+// funcoes que retornam outras funcoes
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 };
 
-// oneWord = oneWord("A patria que te Pariu");
-// console.log(oneWord);
+const greetHey = greet("Hey");
+greetHey("Jonas");
+greetHey("Michael");
+greet("Saudacoes")("Moacir");
 
-// Usando spread operator e destructuring assignment para CAPITALIZAR primeira palavra
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(" ");
-  return [first.toUpperCase(), ...others].join(" ");
-};
+// Escrevendo em arrow function
+const saudacao = (aceno) => (pessoa) => console.log(`${aceno} ${pessoa}`);
 
-// Higher-Order function
-const transformer = function (str, fn) {
-  console.log(`Original: ${str}`);
-  console.log(`Modificado: ${fn(str)}`);
-  console.log(`Transformado por: ${fn.name}`);
-};
-
-transformer("Somente sei que nada sei", upperFirstWord);
-transformer("Somente sei que nada sei", oneWord);
-
-// const teste = "tudo que sobe desce";
-// console.log(teste.split(" "));
-// const [first, ...others] = teste.split(" ");
-// console.log(first);
-// console.log(others);
-// const junta = [first.toUpperCase(), ...others].join(" ");
-// console.log(junta);
-
-const high5 = function () {
-  console.log("🖐🏻");
-};
-
-document.body.addEventListener("click", high5);
-
-["Adam", "Maria", "Tonho"].forEach(high5);
+saudacao("Fala")("Joao");
