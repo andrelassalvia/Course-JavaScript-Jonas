e3b618769264ca9e138ca79923039680c6a9ea20
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-15 20:06:09 +0000

    Metodos CALL e APPLY - Chamando a funcao de um objeto em outro objeto

--- Secao10-Funcoes/script.js
+++ Secao10-Funcoes/script.js
@@ -1,18 +1,46 @@
 "use strict";
 
-// funcoes que retornam outras funcoes
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
+// Objeto
+const lufthansa = {
+  airline: "lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  // funcao como atributo do objeto
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a flight with ${this.airline} flight number ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
 };
 
-const greetHey = greet("Hey");
-greetHey("Jonas");
-greetHey("Michael");
-greet("Saudacoes")("Moacir");
+// chama a funcao do objeto
+lufthansa.book(239, "Mossoro");
+console.log(lufthansa);
 
-// Escrevendo em arrow function
-const saudacao = (aceno) => (pessoa) => console.log(`${aceno} ${pessoa}`);
+const euroWings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
 
-saudacao("Fala")("Joao");
+// Define a constante book como sendo a funcao do primeiro objeto
+const book = lufthansa.book;
+
+// Chamando a funcao desta forma Nao funciona
+// book(23, "Maria");
+
+// Para chamar um metodo de um objeto em outro objeto
+// CALL METHOD
+book.call(euroWings, 23, "Maria Jose");
+book.call(lufthansa, 654, "Tiao Carrero");
+
+console.log(euroWings);
+console.log(lufthansa);
+
+// Outra forma de chamar um metodo de um objeto em outro objeto
+// APPLY METHOD - Segundo argumento deve ser um array
+book.apply(euroWings, [666, "Ze do Capeta"]);
+// or
+const flghtData = [666, "Ze do Capeta"];
+book.call(euroWings, ...flghtData);
