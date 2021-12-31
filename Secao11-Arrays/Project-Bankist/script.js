502bb2031b19d1cb45e4ab4d7e2291288c876068
Andre Lassalvia <andrelassalvia@gmail.com>
2021-12-31 12:37:38 +0000

    Flat, FlatMap and Sorting arrays

--- Secao11-Arrays/Project-Bankist/script.js
+++ Secao11-Arrays/Project-Bankist/script.js
@@ -62,9 +62,19 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 // DISPLAY MOVEMENTS
-const displayMovements = function (movement) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
-  movement.forEach(function (mov, i) {
+  const movs = sort
+    ? movements.slice().sort((a, b) => {
+        if (a < b) {
+          return -1;
+        }
+        if (a > b) {
+          return 1;
+        }
+      })
+    : movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
       <div class="movements__row">
@@ -352,6 +362,13 @@ btnLoan.addEventListener("click", function (e) {
   inputLoanAmount.value = "";
 });
 
+// SORT BUTTON
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
