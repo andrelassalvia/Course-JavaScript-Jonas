27d3e62e9b8c0cac420d6870a79e980b33909366
Grant <github@firefishy.com>
2022-08-28 01:07:39 +0300

    Update script.js
    `{s}.` is no longer recommended now that we support HTTP/2 + HTTP/3.

--- Secao15-Mapty_OPP/starter/script.js
+++ Secao15-Mapty_OPP/starter/script.js
@@ -116,7 +116,7 @@ class App {
     const coords = [latitude, longitude];
     this.#map = L.map('map').setView(coords, 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
